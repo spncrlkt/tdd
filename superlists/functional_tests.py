6537fdf6e6e5cf33c128249da03d04597a12cbd6
Spencer Liechty <sliechty@innography.co>
2013-09-07 16:04:58 -0500

    Move FTs into unittest format

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_reterive_it_later(self):
+        browser = webdriver.Firefox()
+        browser.get('http://localhost:8000')
+
+        # U notices the page title and header mention to do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        # U is invited to enter a to-do item straight away
+
+        # U typus "Buy peacokc feathers" into a text box
+
+        # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
+
+        # Text box intives U to add another item
+
+        # U enters "Use peacock feathers to make a fly"
+
+        # The page updates again, and now shows both items in the list
+
+        # Explanatory exists for the unique url
+
+        # U visits uniq url, the list is still there
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
