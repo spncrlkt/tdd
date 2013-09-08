1e95b863173d2e37000dc23ac55b8c7a5461644e
Spencer Liechty <sliechty@innography.co>
2013-09-07 21:32:08 -0500

    Beginning to implement server side request handling

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -28,16 +29,25 @@ class NewVisitorTest(unittest.TestCase):
 
         # U types "Buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
         # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Text box intives U to add another item
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         self.fail('Finish the test!')
 
         # U enters "Use peacock feathers to make a fly"
