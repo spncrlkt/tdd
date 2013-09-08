cfce7e1ed78bbfca67bdcc6d9e9ff714c13ebe0b
Spencer Liechty <sliechty@innography.co>
2013-09-07 21:47:11 -0500

    Finish functional test refactor

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_reterive_it_later(self):
         self.browser = webdriver.Firefox()
         self.browser.get('http://localhost:8000')
@@ -32,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Text box intives U to add another item
         inputbox.send_keys('Use peacock feathers to make a fly')
@@ -43,10 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
         )
         self.fail('Finish the test!')
 
