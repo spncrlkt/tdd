464f3d3393bc91320539d8e3d41d6da72252c3f3
Spencer Liechty <sliechty@innography.co>
2013-09-08 13:25:18 -0500

    Add next phase of ft's

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -39,6 +39,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Text box intives U to add another item
@@ -46,18 +48,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # When U hits enter, the page lists "1: Buy peacock feathers" in a to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        # U enters "Use peacock feathers to make a fly"
+        # The page updates again, and now shows both items in the list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly'
         )
-        self.fail('Finish the test!')
 
-        # U enters "Use peacock feathers to make a fly"
 
-        # The page updates again, and now shows both items in the list
+
 
         # Explanatory exists for the unique url
 
