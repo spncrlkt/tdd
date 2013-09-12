91170fefed291d6446e4d0cf25970879a07f75b2
Spencer Liechty <sliechty@innography.co>
2013-09-12 18:16:30 -0500

    Add basic styling functional tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -80,3 +80,23 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Explanatory exists for the unique url
         # U visits uniq url, the list is still there
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+
+        inputbox = self.browser.find_element_by_tag_name('input')
+        window_width = self.browser.get_window_size()['width']
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            window_width / 2,
+            delta=3
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_tag_name('input')
+        window_width = self.browser.get_window_size()['width']
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            window_width / 2,
+            delta=3
+        )
