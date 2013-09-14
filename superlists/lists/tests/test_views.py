ba5eb5be730ffc21b97bdf21d24829e7e266e0e5
Spencer Liechty <sliechty@innography.co>
2013-09-14 16:20:28 -0500

    Split unittests into 2 files

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -26,35 +26,6 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.all().count(), 0)
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list = List()
-        list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list
-        second_item.save()
-
-        saved_lists = List.objects.all()
-        self.assertEqual(saved_lists.count(), 1)
-        self.assertEqual(saved_lists[0], list)
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list)
-
 class ListViewTest(TestCase):
 
     def test_list_view_dispalys_all_items(self):
