c7ce436d567bfc6d3647e112f482e81dc59d30d1
Spencer Liechty <sliechty@innography.co>
2013-09-15 23:57:53 -0500

    Add Item validation FT's

--- superlists/functional_tests/tests/test_list_item_validation.py
+++ superlists/functional_tests/tests/test_list_item_validation.py
@@ -4,14 +4,27 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         #tries to submit empty list item
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         #error message
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         #tries to submit valid item, works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         #tries second empty list item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         #error message
+        error = self.browser.find_element_by_css_selector('.error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         #corrects it by adding text
-        self.fail('write me')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
+
+        self.fail("Don't forget to also test duplicate items!")
